b0bdfdb2f876ec7b162811a9188e815ce6f5269b
rtm0x143 <russia-artyom@mail.ru>
2022-02-25 20:35:27 +0700

    redone all

--- models/Model.js
+++ models/Model.js
@@ -1,7 +1,14 @@
-class Model {
-    constructor(title = "", date = "") {
-        this.title = title;
-        this.date = date;
-        this.id = Date.now();
+class TaskList {
+    constructor(baseTaskArray = [], idGetter = Date.now) {
+        this.tasks = baseTaskArray;
+        this.getNewId = idGetter
+    }
+
+    addTask(_title, _date) {
+        this.tasks.push({title: _title, date: _date, id: this.getNewId()})
+    }
+
+    removeById(_id) {
+        this.tasks.remove(this.tasks.find(task => task.id === _id))
     }
 }
