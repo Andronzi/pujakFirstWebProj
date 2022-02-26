2418989eb9a0fdfae798553d81a545ca7638aa9e
rtm0x143 <russia-artyom@mail.ru>
2022-02-26 20:45:03 +0700

    fixed bugs

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -4,11 +4,17 @@ class TaskListController {
         this.taskList = taskList
 
         this.view.bindAddTask(this.handleAddTask)
+        this.view.bindRemTask(this.handleRemTask)
         this.view.bindChangeTask(this.handleChangeTask)
     }
 
     handleAddTask = (title, date) => {
         this.taskList.addTask(title, date)
+        this.onTaskListChanged()
+    }
+
+    handleRemTask = (id) => {
+        this.taskList.removeById(id)
         this.onTaskListChanged()
     }
 
